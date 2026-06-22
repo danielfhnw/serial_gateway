584f81c52e915bd8624b52c4de498b3d084ba58a
danielfhnw <daniel.jakober@fhnw.ch>
2026-06-22 17:48:32 +0200

    updated publish to .env

--- publish_readings.py
+++ publish_readings.py
@@ -1,22 +1,21 @@
 from dataclasses import fields
-
 import serial
-import re
 import json
 import time
-from datetime import datetime
+from datetime import datetime, timezone
 import paho.mqtt.client as mqtt
 from dotenv import load_dotenv
 import os
 
-# ---------- SERIAL ----------
-SERIAL_PORT = "/dev/ttyUSB0"
-BAUD_RATE = 9600
+
+load_dotenv()
+
+
+SERIAL_PORT = os.getenv("SERIAL_PORT")
+BAUD_RATE = int(os.getenv("BAUD_RATE", 9600))
 
 ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
 
-# ---------- MQTT ----------
-load_dotenv()
 
 MQTT_BROKER = os.getenv("MQTT_BROKER")
 MQTT_PORT = int(os.getenv("MQTT_PORT"))
@@ -32,10 +31,6 @@ if MQTT_USER:
 client.connect(MQTT_BROKER, MQTT_PORT, 60)
 client.loop_start()
 
-
-def extract_value(line):
-    match = re.search(r"(-?\d+\.\d+)", line)
-    return float(match.group(1)) if match else None
 
 while True:
     line = ser.readline().decode(errors="ignore").strip()
@@ -53,7 +48,7 @@ while True:
         "weight": float(fields[2]),
         "tare_weight": float(fields[3]),
         "unit": fields[5],
-        "timestamp": datetime.utcnow().isoformat() + "Z"
+        "timestamp": datetime.now(timezone.utc).isoformat()
     }
 
     topic = MQTT_TOPIC
