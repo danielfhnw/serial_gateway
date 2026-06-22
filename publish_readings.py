faba17fed77dc785ea6b98499d4d65e9a5702a5a
danielfhnw <daniel.jakober@fhnw.ch>
2026-06-22 08:25:58 +0200

    updated mqtt connection

--- publish_readings.py
+++ publish_readings.py
@@ -4,22 +4,32 @@ import json
 import time
 from datetime import datetime
 import paho.mqtt.client as mqtt
+from dotenv import load_dotenv
+import os
 
 # ---------- SERIAL ----------
 SERIAL_PORT = "/dev/ttyUSB0"
 BAUD_RATE = 9600
 
-# ---------- PUBLIC MQTT BROKER ----------
-MQTT_BROKER = "test.mosquitto.org"
-MQTT_PORT = 1883
-MQTT_TOPIC = "dj1000/sensor/weight"  # IMPORTANT: make it unique
-
-# ---------- SETUP ----------
 ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
 
+# ---------- MQTT ----------
+load_dotenv()
+
+MQTT_BROKER = os.getenv("MQTT_BROKER")
+MQTT_PORT = int(os.getenv("MQTT_PORT"))
+MQTT_USER = os.getenv("MQTT_USER")
+MQTT_PASSWORD = os.getenv("MQTT_PASSWORD")
+MQTT_TOPIC = os.getenv("MQTT_TOPIC")
+
 client = mqtt.Client()
+
+if MQTT_USER:
+    client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
+
 client.connect(MQTT_BROKER, MQTT_PORT, 60)
 client.loop_start()
+
 
 def extract_value(line):
     match = re.search(r"(-?\d+\.\d+)", line)
@@ -48,4 +58,4 @@ while True:
 
     print("Published:", payload)
 
-    time.sleep(0.1)
+    time.sleep(0.05)
